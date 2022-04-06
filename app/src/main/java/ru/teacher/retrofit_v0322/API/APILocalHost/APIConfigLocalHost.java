961111cb04ac566a078fed413bd653cf0040efd3
redhatdeath <redhatdeath@yandex.ru>
2022-04-06 12:51:01 +0500

    Add Web Node JS. Request body work

--- app/src/main/java/ru/teacher/retrofit_v0322/API/APILocalHost/APIConfigLocalHost.java
+++ app/src/main/java/ru/teacher/retrofit_v0322/API/APILocalHost/APIConfigLocalHost.java
@@ -1,5 +1,5 @@
 package ru.teacher.retrofit_v0322.API.APILocalHost;
 
 public class APIConfigLocalHost {
-    public static final String HOST_URL = "http://192.168.253.160:8081/";
+    public static final String HOST_URL = "http://192.168.*.*:8081/";
 }
